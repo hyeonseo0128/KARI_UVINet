7511de4c4e0a633ef0f11758f2e5789320df2765
hyeonseo0128 <sseokgom0128@gmail.com>
2023-06-08 23:09:32 +0900

    - IP config 파일 분리

--- acm_uvinet.js
+++ acm_uvinet.js
@@ -1,14 +1,15 @@
 const dgram = require('dgram');
 const { SerialPort } = require('serialport');
 const os = require('os');
+const fs = require("fs");
 const broadcastAddress = require('broadcast-address');
 
 const UCAST_HOST = '127.0.0.1';
-const UCAST_PORT = 20001; 
+const UCAST_PORT = 20001;
 const BCAST_HOST = broadcastAddress('eth0');
-const BCAST_PORT = 20002; 
-const SERIAL_PORT = '/dev/ttyTHS1'; 
-const BAUD_RATE = 115200; 
+const BCAST_PORT = 20002;
+const SERIAL_PORT = '/dev/ttyTHS1';
+const BAUD_RATE = 115200;
 
 const udpSocket = dgram.createSocket('udp4');
 const bcastSocket = dgram.createSocket('udp4');
@@ -22,14 +23,17 @@ const net_int = os.networkInterfaces();
 const MY_IP_ADDRESS = net_int['eth0'][0].address;
 console.log('my ip address:', MY_IP_ADDRESS);
 
-let id_ip_dic = {1: '172.30.10.30', 2: '172.30.10.30', 3: '172.30.10.30', 255: '172.30.10.255',
-                 4: '172.30.10.30', 5: '172.30.10.30', 6: '172.30.10.30', 7: '172.30.10.255',
-                 8: '172.30.10.30', 9: '172.30.10.30', 10: '172.30.10.30'};
+let id_ip_dic = {};
+try {
+    id_ip_dic = JSON.parse(fs.readFileSync('./id_ip_dic.json', 'utf8'));
+} catch (e) {
+    console.log('id_ip_dic file not exist.');
+}
 console.log(id_ip_dic);
 
 udpSocket.on('message', (message, rinfo) => {
     console.log('udpsocket received:', rinfo.address, rinfo.port, 'message:', message.toString('hex'));
-    if (rinfo.address ==  MY_IP_ADDRESS) {
+    if (rinfo.address == MY_IP_ADDRESS) {
         console.log('ucasts my ip address');
     } else {
         console.log('ucast other ip address');
@@ -65,38 +69,38 @@ serialPort.on('data', (data) => {
     console.log('serial received:' + data.toString('hex'));
     size = data.length
     //console.log('size:',size);
-    if (size > 0){
+    if (size > 0) {
         count = count + 1;
         buffer_array = Buffer.concat([buffer_array, data]);
         //console.log('buffer', buffer_array);
-        for (i = 0; i < buffer_array.length; i++){
-            if (i + 2 < buffer_array.length){
-                if (buffer_array[i] == 0xaa && buffer_array[i+1] == 0x55){
+        for (i = 0; i < buffer_array.length; i++) {
+            if (i + 2 < buffer_array.length) {
+                if (buffer_array[i] == 0xaa && buffer_array[i + 1] == 0x55) {
                     //console.log("###init###");
                     //console.log('i value', i, 'barray - (i)', buffer_array.length - (i), 'length value', buffer_array[i+2]);
-                    if ((buffer_array.length - (i)) >= buffer_array[i+2] + 12){
-                        k = buffer_array[i+2] + 12;
+                    if ((buffer_array.length - (i)) >= buffer_array[i + 2] + 12) {
+                        k = buffer_array[i + 2] + 12;
                         //console.log('k value', k);
-                        
-                        if (i > 0){
+
+                        if (i > 0) {
                             console.log('####### before del', buffer_array);
                             buffer_array = buffer_array.slice(i, buffer_array.length);
                             console.log('####### after del', buffer_array);
                             break;
                         }
-                        
-                        if (i + k <= buffer_array.length){
+
+                        if (i + k <= buffer_array.length) {
                             //console.log('parsing');
                             dl = buffer_array.slice(i, k);
                             //console.log('parsing data', dl.toString('hex'));
                             buffer_array = buffer_array.slice(k, buffer_array.length);
 
-                            if (dl.length > 0){
+                            if (dl.length > 0) {
                                 console.log('header1', dl[0].toString(16), 'header2', dl[1].toString(16), 'payload length', dl[2], 'packet sequence', dl[3], 'source ID', dl[4], 'port', dl[5], 'destination ID', dl[6],
-                                            'port', dl[7], 'packet priority', dl[8], 'message Id', dl[9]);
-                            
+                                    'port', dl[7], 'packet priority', dl[8], 'message Id', dl[9]);
+
                                 id = dl[6];
-                                if (id == 255){
+                                if (id == 255) {
                                     ip = id_ip_dic[id.toString()];
                                     console.log('id', id, 'ip', ip);
                                     //send_broadcast();
@@ -108,7 +112,7 @@ serialPort.on('data', (data) => {
 
                                     break;
                                 }
-                                
+
                                 ip = id_ip_dic[id.toString()];
                                 console.log('id', id, 'ip', ip);
                                 //send_unicast();
@@ -120,7 +124,7 @@ serialPort.on('data', (data) => {
                             }
                         }
                     }
-                }           
+                }
             }
         }
     }
